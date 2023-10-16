e991e1ab88f43973463af53e83b7046db120b54b
TaiShow0121 <148000013+TaiShow0121@users.noreply.github.com>
2023-10-16 15:47:58 +0900

    Update turtle_1.py

--- turtle_1.py
+++ turtle_1.py
@@ -1,9 +1,10 @@
 import turtle
+
 n = float(input(">"))
-turtle.speed(1)
+turtle.speed(0) #描画速度表示
 d = 360/n
-for i in range(2000):
-    turtle.forward(100)
-    turtle.left(d)
+for i in range(2000):　
+    turtle.forward(200)　#線の長さ
+    turtle.left(d)　#角度
 
 turtle.done()
